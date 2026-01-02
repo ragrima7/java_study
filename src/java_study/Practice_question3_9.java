3397567cc98f9d8ae75ae75bd66e782a3d3a8794
ragrima7 <ragrima7@gmail.com>
2026-01-02 22:15:39 +0900

    Practice_question3_9の回答

---
+++ src/java_study/Practice_question3_9.java
@@ -0,0 +1,12 @@
+package java_study;
+
+public class Practice_question3_9 {
+    
+    public static void main(String[] args) {
+        int num = (int)(Math.random()*100) + 1;
+        System.out.println("num = " + num);
+        if(num <= 50) {
+            System.out.println("です");
+        }
+    }
+}
