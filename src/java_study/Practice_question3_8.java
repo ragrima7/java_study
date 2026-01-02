d84b28e781adf6f872f0b2f9138d9d76d3f4be3b
ragrima7 <ragrima7@gmail.com>
2026-01-02 21:32:28 +0900

    Practice_question3_8の回答

---
+++ src/java_study/Practice_question3_8.java
@@ -0,0 +1,14 @@
+package java_study;
+
+public class Practice_question3_8 {
+    
+    public static void main(String[] args) {
+        int num = (int)(Math.random()*10) + 1;
+        System.out.println("num = " + num);
+        if(num == 1) {
+            System.out.println("1です");
+        }else if(num != 1) {
+            System.out.println("1ではありません");
+    }
+}
+}
