ef12954e105ce3f399908c171ccb6ecfb249cc97
ragrima7 <ragrima7@gmail.com>
2026-01-03 19:29:04 +0900

    問）4_5回答完了

---
+++ src/java_study/Practice_question4_5.java
@@ -0,0 +1,17 @@
+package java_study;
+
+//for文を用いて、変更し、表示するプログラムを作りなさい。
+//a = 0
+//a = 1
+//a = 2
+//a = 3
+
+public class Practice_question4_5 {
+    
+    public static void main(String[] args) {
+        int a = 0;
+        for (; a <= 3; a++) {
+            System.out.println("a = " + a); 
+        }
+    }
+    }
