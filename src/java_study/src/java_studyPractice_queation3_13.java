3f9833bb3e1e0db9578f3e3d17d76b104622259a
ragrima7 <ragrima7@gmail.com>
2026-01-03 10:27:30 +0900

    Practice_queation3_13の回答

---
+++ src/java_study/src/java_studyPractice_queation3_13.java
@@ -0,0 +1,16 @@
+package java_study.src;
+
+public class java_studyPractice_queation3_13 {
+    
+    public static void main(String[] args) {
+        int num = (int)(Math.random()*3 + 1);
+        System.out.println("num = " + num);
+        if (num == 1) 
+            System.out.println("グー");
+        else if (num == 2) 
+            System.out.println("チョキ");
+        else if(num == 3) {
+            System.out.println("パー");
+        }
+}
+}
