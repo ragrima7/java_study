e1c3fddf7ce1c13095eecf218f3847495fa059c2
ragrima7 <ragrima7@gmail.com>
2026-01-03 20:54:25 +0900

    演習問題のファイル名誤りを修正（4_10→4_9、4_11→4_10）

--- src/java_study/Practice_question4_10.java
+++ src/java_study/Practice_question4_10.java
@@ -1,19 +1,14 @@
 package java_study;
 
-//whileの無限ループを作り、その中で、
-// １から１００までの乱数を発生させ、表示しなさい。
-// その際、値が１０の倍数であれば、
-// ループを抜け、”10の倍数が発生したので終了します”と表示して、
-// プログラムを終了しなさい。
+//forループを用いて、１から100までの乱数を５回発生させ、表示させなさい.
 
-public class Practice_question4_10 {
+public class Practice_question4_11 {
     
     public static void main(String[] args) {
-        int num = 1;
-        while (num % 10 != 0) {
-            num = (int) (Math.random() * 100) + 1;
+
+        for (int i = 0; i < 5; i++) {
+            int num = (int) (Math.random() * 100) + 1;
             System.out.println(num);
             }
-        System.out.println("10の倍数が発生したので、終了します");
-    }
+}
 }
