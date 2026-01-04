b07a595980dca16c47e303e5be18230771d2d63b
ragrima7 <ragrima7@gmail.com>
2026-01-04 16:07:10 +0900

    問）4_18

---
+++ src/java_study/Practice_question4_18.java
@@ -0,0 +1,18 @@
+package java_study;
+
+public class Practice_question4_18 {
+    
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+            
+                if (i == j) {
+                    System.out.print("□ ");
+                } else {
+                    System.out.print("■ ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
