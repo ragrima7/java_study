2046bc74c90ece3602c6950253019520e50f5685
ragrima7 <ragrima7@gmail.com>
2026-01-05 20:50:13 +0900

    Sample.501理解済み

---
+++ src/java_study/Sample501.java
@@ -0,0 +1,18 @@
+package java_study;
+
+public class Sample501 {
+
+    public static void main(String[] args) {
+        
+        double one, two, three;
+        double sum,avg;
+        one = 1.2;
+        two = 3.7;
+        three = 4.1;
+        System.out.println(one + " " + two + " " + three);
+        sum = one + two + three;
+        avg = sum / 3.0;
+        System.out.println("合計: " + sum);
+        System.out.println("平均: " + avg);
+    }
+}
