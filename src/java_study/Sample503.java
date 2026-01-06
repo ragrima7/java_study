4ba55adf4c84ac25c4926b5939b7890216207aca
ragrima7 <ragrima7@gmail.com>
2026-01-06 21:58:08 +0900

    Sample503理解済

---
+++ src/java_study/Sample503.java
@@ -0,0 +1,19 @@
+package java_study;
+
+public class Sample503 {
+    
+    public static void main(String[] args) {
+        
+        int n[] = {5,4,3,2,1};
+        String s[] = {"abc","def","efg"};
+        int i;
+        for(i = 0; i < n.length; i++) {
+            System.out.println(n[i] + " ");
+        }
+        System.out.println();
+        for(i = 0; i < s.length; i++) {
+            System.out.println(s[i] + " ");
+        }
+        System.out.println();
+    }
+}
